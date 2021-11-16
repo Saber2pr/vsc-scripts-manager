6d962d31ef090eac16478d22bac340af064389ee
saber2pr <saber2pr@gmail.com>
2021-11-16 19:31:31 +0800

    feat: terminal singleton

--- src/utils/runner.ts
+++ src/utils/runner.ts
@@ -10,13 +10,25 @@ const Runner = {
   '.ts': 'ts-node',
 }
 
+const RunnerName = 'scripts-runner'
+
+const getTerminal = () => {
+  if (window.terminals && window.terminals.length > 0) {
+    const result = window.terminals.find(item => item.name === RunnerName)
+    if (result) {
+      return result
+    }
+  }
+  return window.createTerminal(RunnerName)
+}
+
 export const runScript = (path: string, args: string[]) => {
   const scriptPath = resolve(path)
   const { ext } = parse(path)
   try {
     readFileSync(scriptPath)
     if (ext in Runner) {
-      const terminal = window.createTerminal()
+      const terminal = getTerminal()
       terminal.show()
       terminal.sendText(
         `${Runner[ext]} ${scriptPath} ${getArray(args).join(' ')}`
